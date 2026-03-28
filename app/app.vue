f142a52711c7b5601de060267745a6049b4de93b
JPITH <87413188+JPITH@users.noreply.github.com>
2026-03-28 17:53:19 +0100

    Setup project scaffolding and styling

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-background">
+    <NuxtPage />
+    <ClientOnly>
+      <Toaster position="top-center" rich-colors />
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Toaster } from 'vue-sonner'
+import 'vue-sonner/style.css'
+</script>
