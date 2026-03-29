6cb9df99a478992d9d7657e7972e792ab8ba27e8
JPITH <87413188+JPITH@users.noreply.github.com>
2026-03-29 10:48:21 +0100

    chore: bump application version to v1.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
     public: {
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:4000',
       appName: 'Carerpoint Family Portal',
-      appVersion: 'v1.0',
+      appVersion: 'v1.1',
     },
   },
 
